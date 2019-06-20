aeaccf2a9fe40cad2627245c9b755b780069cbd7
Hossein <51873658+irhosseinz@users.noreply.github.com>
2019-06-21 04:12:59 +0430

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,61 @@
+Skip to content
+ irhosseinz / MANAGER
+Code  Issues 0  Pull requests 0  Projects 0  Security
+MANAGER/backup.sh
+ root first commit
+a26a934 2 days ago
+44 lines (37 sloc)  1.37 KB
+  
+#!/bin/bash
+APP="Project name"
+
+function upload_ftp(){
+	curl -T $1 ftp://backup_space_ip:21/$2 --user username:password
+}
+
+DATE=$(date +"%F_%H.%M.%S")
+ROOT="/home/user"
+cd $ROOT
+
+#backup files
+FILE="$APP.files.$DATE.tar.gz"
+SAVEPATH="backups/$FILE"
+echo "creating backup file $SAVEPATH for files"
+tar -czf $SAVEPATH public_html # add folders you want to backup
+FILESIZE=$(stat -c%s "$SAVEPATH")
+echo "file created. size:$FILESIZE"
+upload_ftp $SAVEPATH "$APP/$FILE"
+
+#backup mongodb database
+FILE="$APP.APP.DB_$DATE.archive.gz"
+SAVEPATH="backups/$FILE"
+echo "creating backup file $SAVEPATH for db"
+/usr/bin/mongodump --uri=mongodb://127.0.0.1:27017/dbname --gzip --archive=$SAVEPATH
+FILESIZE=$(stat -c%s "$SAVEPATH")
+echo "file created. size:$FILESIZE"
+upload_ftp $SAVEPATH "$APP/$FILE"
+
+#backup sql satabase
+FILE="$APP.DB_$DATE.sql.gz"
+SAVEPATH="backups/$FILE"
+echo "creating backup file $SAVEPATH for db"
+mysqldump -u username -ppassword --master-data=2 --single-transaction -q --databases db1 db2 db3 | gzip > $SAVEPATH
+FILESIZE=$(stat -c%s "$SAVEPATH")
+echo "file created. size:$FILESIZE"
+upload_ftp $SAVEPATH "$APP/$FILE"
+
+#clear old files
+find backups -type f -iname "*.tar.gz" -mtime +3 -exec rm {} \;
+find backups -type f -iname "*.sql.gz" -mtime +3 -exec rm {} \;
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
