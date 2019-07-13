5e59e132cb15703710c6ef5c8ac6e7d83ad4cb34
Hossein <51873658+irhosseinz@users.noreply.github.com>
2019-07-13 14:26:23 +0430

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,41 +1,47 @@
 #!/bin/bash
-APP="Project name"
+APP="Project"
 
 function upload_ftp(){
-	curl -T $1 ftp://backup_space_ip:21/$2 --user username:password
+	FTP_SERVER="1.2.3.4:21"
+	FTP_USER="ftpuser"
+	FTP_PASS="pass"
+	curl -T $1 ftp://$FTP_SERVER/$2 --user $FTP_USER:$FTP_PASS
 }
 
-DATE=$(date +"%F_%H.%M.%S")
-ROOT="/home/user"
+DATE="LAST"
+#DATE=$(date +"%F_%H.%M.%S") #comment this to don't make a file for each execution 
+ROOT=$(cd `dirname $0` && pwd)
 cd $ROOT
+mkdir -p backups
 
 #backup files
-FILE="$APP.files.$DATE.tar.gz"
+FILE="$APP.files.$DATE.tar.gz" # Project.files.2019-07-11_05.05.01.tar.gz
 SAVEPATH="backups/$FILE"
 echo "creating backup file $SAVEPATH for files"
-tar -czf $SAVEPATH public_html # add folders you want to backup
+tar -czf $SAVEPATH folder1 folder2 file1 file2. # add folders you want to backup
 FILESIZE=$(stat -c%s "$SAVEPATH")
 echo "file created. size:$FILESIZE"
-upload_ftp $SAVEPATH "$APP/$FILE"
-
-#backup mongodb database
-FILE="$APP.APP.DB_$DATE.archive.gz"
-SAVEPATH="backups/$FILE"
-echo "creating backup file $SAVEPATH for db"
-/usr/bin/mongodump --uri=mongodb://127.0.0.1:27017/dbname --gzip --archive=$SAVEPATH
-FILESIZE=$(stat -c%s "$SAVEPATH")
-echo "file created. size:$FILESIZE"
-upload_ftp $SAVEPATH "$APP/$FILE"
+upload_ftp $SAVEPATH "$FILE"
 
 #backup mysql satabase
-FILE="$APP.DB_$DATE.sql.gz"
+FILE="$APP.DB.$DATE.sql.gz" # Project.DB.2019-07-11_05.05.01.sql.gz
 SAVEPATH="backups/$FILE"
 echo "creating backup file $SAVEPATH for db"
-mysqldump -u username -ppassword --master-data=2 --single-transaction -q --databases db1 db2 db3 | gzip > $SAVEPATH
+mysqldump -u username -ppassword --single-transaction -q --databases db1 db2 db3 | gzip > $SAVEPATH
+#mysqldump -u username -ppassword --master-data=2 --single-transaction -q --databases db1 db2 db3 | gzip > $SAVEPATH
 FILESIZE=$(stat -c%s "$SAVEPATH")
 echo "file created. size:$FILESIZE"
-upload_ftp $SAVEPATH "$APP/$FILE"
+upload_ftp $SAVEPATH "$FILE"
+
+#backup mongodb database
+#FILE="$APP.APP.DB.$DATE.archive.gz" # Project.DB.2019-07-11_05.05.01.archive.gz
+#SAVEPATH="backups/$FILE"
+#echo "creating backup file $SAVEPATH for db"
+#/usr/bin/mongodump --uri=mongodb://127.0.0.1:27017/dbname --gzip --archive=$SAVEPATH
+#FILESIZE=$(stat -c%s "$SAVEPATH")
+#echo "file created. size:$FILESIZE"
+#upload_ftp $SAVEPATH "$FILE"
 
 #clear old files
-find backups -type f -iname "*.tar.gz" -mtime +3 -exec rm {} \;
-find backups -type f -iname "*.sql.gz" -mtime +3 -exec rm {} \;
+find backups -type f -iname "*.tar.gz" -mtime +3 -exec rm {} \; #clean .tar.gz files older than 3 days
+find backups -type f -iname "*.sql.gz" -mtime +3 -exec rm {} \; #clean .sql.gz files older than 3 days
